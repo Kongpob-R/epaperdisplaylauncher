83f07036d2a14aa7297b80fe98ff65d5459bc3e9
Kongpob-R <kongpop.rak@hotmail.co.th>
2022-03-18 22:49:33 +0700

    implement deviceApps to homePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:device_apps/device_apps.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +9,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Application> apps = [];
+
+  void getApp() async {
+    apps = await DeviceApps.getInstalledApplications(
+      includeAppIcons: true,
+      includeSystemApps: true,
+      onlyAppsWithLaunchIntent: true,
+    );
+  }
+
+  @override
+  void initState() {
+    getApp();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home'),
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: apps.length,
+      itemBuilder: (BuildContext context, int index) {
+        return SizedBox(
+          height: 50,
+          child: Center(child: Text('Entry ${apps[index].appName}')),
+        );
+      },
     );
   }
 }
