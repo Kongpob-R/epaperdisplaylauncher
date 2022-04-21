a2e21ba44fcab4d2af0e093213375d8df4d28884
Kongpob-R <kongpop.rak@hotmail.co.th>
2022-04-21 16:53:42 +0700

    add library books browse from multiple subDir

--- lib/library_page.dart
+++ lib/library_page.dart
@@ -1,6 +1,9 @@
 import 'package:epaperdisplaylauncher/epub_viewer.dart';
+import 'package:epub_view/epub_view.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as path;
+import 'dart:io' as io;
 // import 'package:path_provider/path_provider.dart';
 
 class LibraryPage extends StatefulWidget {
@@ -11,9 +14,14 @@ class LibraryPage extends StatefulWidget {
 }
 
 class _LibraryPageState extends State<LibraryPage> {
-  late String? filePath;
+  late String filePath;
+  String rootDirectory = '/storage/emulated/0';
+  List files = [];
+  final ItemScrollController itemScrollController = ItemScrollController();
+  final ItemPositionsListener itemPositionsListener =
+      ItemPositionsListener.create();
 
-  void launchReader() async {
+  void launchReader(String filePath) async {
     // final _result = await OpenFile.open('assets/books/bitcoin_standard.epub');
     // Directory? storageDir = await getExternalStorageDirectory()
     // filePath = storageDir!.path;
@@ -22,36 +30,80 @@ class _LibraryPageState extends State<LibraryPage> {
     //   'Books',
     //   'Cartoons on the War1248',
     // );
-    filePath = path.join(
-      '/storage/emulated/0',
-      'Books',
-      'Cartoons on the War1248.epub',
-    );
     Navigator.push(
       context,
       PageRouteBuilder(
         pageBuilder: (context, animation1, animation2) =>
-            EpubViewer(filePath: filePath!),
+            EpubViewer(filePath: filePath),
         transitionDuration: Duration.zero,
         reverseTransitionDuration: Duration.zero,
       ),
     );
   }
 
+  void _listBooks(List<String> subDirectories) {
+    for (var subDirectory in subDirectories) {
+      final targetPath = io.Directory(path.join(
+        rootDirectory,
+        subDirectory,
+      ));
+      if (targetPath.existsSync()) {
+        files += targetPath.listSync(
+          recursive: true,
+          followLinks: false,
+        );
+      }
+    }
+    setState(() {
+      files = files
+          .where((element) =>
+              element.path.contains('.epub') || element.path.contains('.pdf'))
+          .toList();
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    _listBooks(['Books', 'Books/MoonReader']);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    files = [];
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(50),
       child: Column(
         children: <Widget>[
-          ElevatedButton(
-            onPressed: launchReader,
-            child: const Text('launchReader'),
+          Expanded(
+            child: ScrollablePositionedList.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemCount: files.length,
+              itemScrollController: itemScrollController,
+              itemPositionsListener: itemPositionsListener,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    launchReader(files[index].path);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      border: Border(bottom: BorderSide(width: 1)),
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Text(files[index].path),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
